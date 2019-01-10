4f7f537564f62e90b76d4cd166e09e80ecd2fbe3
LeeSD90 <lee.doyle90@gmail.com>
2019-01-10 13:41:00 +0100

    remove mail domain var from action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: 587,
-  domain: ENV["MAIL_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["SENDGRID_USERNAME"],
