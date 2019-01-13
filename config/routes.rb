fadbc524d750d666285322886da9a4a27705dd26
LeeSD90 <lee.doyle90@gmail.com>
2019-01-13 11:30:35 +0100

    rarrange routes and remove test index.html from public

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'landing#new'
+  devise_for :users
   match '/landing', to: 'landing#new', via: 'get'
   resources "landing", only: [:new, :create]
 end
