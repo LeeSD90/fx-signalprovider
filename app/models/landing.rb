0a3c4041dcad883021b64bcba9a8119786777374
LeeSD90 <lee.doyle90@gmail.com>
2019-01-19 11:08:52 +0100

    add country selector

--- app/models/landing.rb
+++ app/models/landing.rb
@@ -1,6 +1,7 @@
 class Landing < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :country
   attribute :message
   attribute :phone
   attribute :call_me
