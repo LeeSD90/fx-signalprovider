d98d6c0e2a3bd31fcd38cfa39d838dccb51ed17f
LeeSD90 <lee.doyle90@gmail.com>
2019-01-21 12:50:03 +0100

    add static pages show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'landing#new'
   devise_for :users
+
   match '/landing', to: 'landing#new', via: 'get'
   resources "landing", only: [:new, :create]
+
+  get "/static_pages/:static_page" => "static_pages#show"
 end
