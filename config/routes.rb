f17b7560ab16d01ae051bf50a8db4af90dc6eb9a
LeeSD90 <lee.doyle90@gmail.com>
2019-01-31 11:04:41 +0100

    add back static pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources "landing", only: [:new, :create]
 
   devise_for :users
-  get '*path' => "application#render_404"
   get "/:static_page" => "static_pages#show", :as => "static_page"
+  get '*path' => "application#render_404"
 
 end
