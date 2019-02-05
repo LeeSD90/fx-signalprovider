56877ad50b0f630e7f961cbc3668e240da567f0d
LeeSD90 <lee.doyle90@gmail.com>
2019-02-05 15:23:15 +0100

    add subscribed? helper method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one :subscription, :dependent => :destroy
+
+  def subscribed?
+    !self.subscription.nil?
+  end
 end
