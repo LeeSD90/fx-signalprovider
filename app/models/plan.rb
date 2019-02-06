257230f76cc73ba30b75017e32fb019c6a8a34e2
LeeSD90 <lee.doyle90@gmail.com>
2019-02-06 12:32:44 +0100

    fix model name in plan validations

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,6 @@
 class Plan < ApplicationRecord
   NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at"]
-  VALIDATABLE_ATTRS = YourModel.attribute_names.reject{|attr| NON_VALIDATABLE_ATTRS.include?(attr)}
+  VALIDATABLE_ATTRS = Plan.attribute_names.reject{|attr| NON_VALIDATABLE_ATTRS.include?(attr)}
 
   validates_presence_of VALIDATABLE_ATTRS
 end
