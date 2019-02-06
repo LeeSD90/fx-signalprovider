e1fd9781ad299fbc2f006896a53066f6fbd35a6f
LeeSD90 <lee.doyle90@gmail.com>
2019-02-06 12:26:18 +0100

    run db:migrate for plans table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205135911) do
+ActiveRecord::Schema.define(version: 20190206112318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.string "interval"
+    t.integer "duration"
+    t.string "currency"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.bigint "user_id"
