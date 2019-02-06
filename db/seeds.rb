52c365da00fa10b8a4e51554b03bd8eb0c1d05c5
LeeSD90 <lee.doyle90@gmail.com>
2019-02-06 12:34:24 +0100

    add db seed data for admin accounts and pricing plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(
+  :email                 => ENV["ADMIN1_MAIL"],
+  :password              => ENV["ADMIN1_PASSWORD"],
+  :password_confirmation => ENV["ADMIN1_PASSWORD"],
+  :admin                 => true
+)
+user.skip_confirmation!
+user.save!
+
+user = User.new(
+  :email                 => ENV["ADMIN2_MAIL"],
+  :password              => ENV["ADMIN1_PASSWORD"],
+  :password_confirmation => ENV["ADMIN1_PASSWORD"],
+  :admin                 => true
+)
+user.skip_confirmation!
+user.save!
+
+plan = Plan.create(
+  :name       => "1 Month Plan",
+  :interval   => "month",
+  :duration   => 1,
+  :currency   => "USD",
+  :price      => 60
+)
+
+plan = Plan.create(
+  :name       => "3 Month Plan",
+  :interval   => "month",
+  :duration   => 3,
+  :currency   => "USD",
+  :price      => 340
+)
+
+plan = Plan.create(
+  :name       => "6 Month Plan",
+  :interval   => "month",
+  :duration   => 6,
+  :currency   => "USD",
+  :price      => 680
+)
