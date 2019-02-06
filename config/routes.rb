6561dc9a094c8214fe2aacf40cb9b908ae3f2446
LeeSD90 <lee.doyle90@gmail.com>
2019-02-06 11:34:00 +0100

    add new and paypal checkout subscriptions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'landing#new'
-  resources "landing", only: [:new, :create]
-
+  resources :landing, only: [:new, :create]
+  resources :subscriptions, only: :new
   devise_for :users
   get "/:static_page" => "static_pages#show", :as => "static_page"
+  get "paypal/checkout", to: "subscriptions#paypal_checkout"
   get '*path' => "application#render_404"
 
 end
