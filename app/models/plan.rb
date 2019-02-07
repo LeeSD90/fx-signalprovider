112392dd14bf2c65cff4c10b9ed36d8e89933352
LeeSD90 <lee.doyle90@gmail.com>
2019-02-07 11:48:15 +0100

    add subscriptions association to plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,10 @@
 class Plan < ApplicationRecord
+  has_many :subscriptions
+  
   NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at"]
   VALIDATABLE_ATTRS = Plan.attribute_names.reject{|attr| NON_VALIDATABLE_ATTRS.include?(attr)}
 
   validates_presence_of VALIDATABLE_ATTRS
+
+
 end
