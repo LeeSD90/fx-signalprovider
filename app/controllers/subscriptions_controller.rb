969597092156301453b446ed92324694bfb16cf2
LeeSD90 <lee.doyle90@gmail.com>
2019-02-11 13:45:11 +0100

    fix cancel and return urls and save on successful payment processing

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -10,24 +10,26 @@ class SubscriptionsController < ApplicationController
     if params[:PayerID]
       @subscription.paypal_customer_token = params[:PayerID]
       @subscription.paypal_payment_token = params[:token]
+      @subscription.save_with_paypal_payment
     end
   end
 
   def paypal_checkout
     plan = Plan.find(params[:plan_id])
     ppr = PayPal::Recurring.new(
-      return_url: new_subscription_path(:plan_id => plan.id),
-      cancel_url: root_path,
+      return_url: new_subscription_url(:plan_id => plan.id),
+      cancel_url: root_url,
       description: plan.name,
       amount: plan.price,
       currency: plan.currency 
     )
 
+    puts ppr.inspect
     response = ppr.checkout
 
     if response.valid?
       redirect_to response.checkout_url
-    else
+    else 
       raise response.errors.inspect
     end
   end
