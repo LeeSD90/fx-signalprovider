e434aa142666ec7d6e2957e0c4627b50e9bb1663
LeeSD90 <lee.doyle90@gmail.com>
2019-02-11 11:48:24 +0100

    add skeleton paypal_checkout controller action

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,6 +9,22 @@ class SubscriptionsController < ApplicationController
   end
 
   def paypal_checkout
+    plan = Plan.find(params[:plan_id])
+    ppr = Paypal::Recurring.new(
+      return_url: ,
+      cancel_url: ,
+      description: ,
+      amount: ,
+      currency: 
+    )
+
+    response = ppr.checkout
+
+    if response.valid?
+      redirect_to response.checkout_url
+    else
+      rais response.errors.inspect
+    end
   end
 
 end
