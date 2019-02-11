a83ebeacbbde6ebc11744d7a770466193c636109
LeeSD90 <lee.doyle90@gmail.com>
2019-02-11 13:44:49 +0100

    add save_with_paypal_payment method

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,20 @@ class Subscription < ApplicationRecord
 
   belongs_to :user
   belongs_to :plan
+
+  def save_with_paypal_payment
+    ppr = PayPal::Recurring.new(
+      token: paypal_payment_token,
+      payer_id: paypal_customer_token,
+      description: plan.name,
+      amount: plan.price,
+      currency: plan.currency
+    )
+
+    response = ppr.request_payment
+    response.inspect
+    if response.errors.present?
+      raise response.errors.inspect
+    end
+  end
 end
