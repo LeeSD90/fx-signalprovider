837549dcf71a75a44b589224808106ec24a3cfae
LeeSD90 <lee.doyle90@gmail.com>
2019-02-11 12:03:21 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190206135443) do
+ActiveRecord::Schema.define(version: 20190211110120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 20190206135443) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "plans_id"
-    t.index ["plans_id"], name: "index_subscriptions_on_plans_id"
+    t.bigint "plan_id"
+    t.index ["plan_id"], name: "index_subscriptions_on_plan_id"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
@@ -51,6 +51,6 @@ ActiveRecord::Schema.define(version: 20190206135443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "subscriptions", "plans", column: "plans_id"
+  add_foreign_key "subscriptions", "plans"
   add_foreign_key "subscriptions", "users"
 end
