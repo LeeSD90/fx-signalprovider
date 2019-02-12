6bae5be3e113d205f2a0f1a9c825d6edf7b4c400
LeeSD90 <lee.doyle90@gmail.com>
2019-02-12 12:56:59 +0100

    add skeleton code for IPN controller

--- app/controllers/paypal_payment_notifications_controller.rb
+++ app/controllers/paypal_payment_notifications_controller.rb
@@ -1,2 +1,21 @@
 class PaypalPaymentNotificationsController < ApplicationController
+  def create
+    response = validate_IPN_notification(request.raw_post)
+
+    case response
+    when "VERIFIED"
+      puts response.inspect
+    when "INVALID"
+      puts response.inspect
+    else
+      #error
+    end
+
+    render :nothing => true
+  end
+
+  protected
+
+    def validate_IPN_notification(raw)
+    end
 end
