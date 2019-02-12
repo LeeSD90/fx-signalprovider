7cb4513176e1b5baaa810f470234388bb3392e1c
LeeSD90 <lee.doyle90@gmail.com>
2019-02-12 14:22:12 +0100

    add ipn notification validation implementation

--- app/controllers/paypal_payment_notifications_controller.rb
+++ app/controllers/paypal_payment_notifications_controller.rb
@@ -17,5 +17,17 @@ class PaypalPaymentNotificationsController < ApplicationController
   protected
 
     def validate_IPN_notification(raw)
+      uri = URI.parse('https://ipnpb.sandbox.paypal.com/cgi-bin/webscr')
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.open_timeout = 60
+      http.read_timeout = 60
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.use_ssl = true
+      response = http.post(
+        uri.request_uri,
+        raw,
+        'Content-Length' => "#{raw.size}",
+        'User-Agent' => "Custom User Agent"
+      ).body
     end
 end
