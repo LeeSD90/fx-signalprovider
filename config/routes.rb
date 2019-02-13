2b33b8d74ea1ca8a7992b50725984ace2af990c5
LeeSD90 <lee.doyle90@gmail.com>
2019-02-13 12:40:01 +0100

    add unsubscribe route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'landing#new'
   resources :landing, only: [:new, :create]
   resources :subscriptions, only: :new
-  resources :paypal_payment_notifications, only: [:create]
+  resources :paypal_payment_notifications, only: :create
   devise_for :users
 
+  get '/unsubscribe', to: "subscriptions#destroy"
   get "/subscription", to: "subscriptions#show"
   get "/signals", to: "shop#signals"
   get "/services", to: "shop#services"
