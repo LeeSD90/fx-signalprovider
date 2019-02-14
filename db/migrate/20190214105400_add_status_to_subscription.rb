7ac518092c6661d7c17b0d6dfce1470678b4b505
LeeSD90 <lee.doyle90@gmail.com>
2019-02-14 12:39:36 +0100

    update status migration to include prevent null values

--- db/migrate/20190214105400_add_status_to_subscription.rb
+++ db/migrate/20190214105400_add_status_to_subscription.rb
@@ -1,5 +1,5 @@
 class AddStatusToSubscription < ActiveRecord::Migration[5.1]
   def change
-    add_column :subscriptions, :status, :integer, default: 0
+    add_column :subscriptions, :status, :integer, null: false, default: 0
   end
 end
