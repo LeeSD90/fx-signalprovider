0ee78a84dcff74bf096930094c87103ece53ef22
LeeSD90 <lee.doyle90@gmail.com>
2019-02-14 12:11:52 +0100

    modify status migration to integer

--- db/migrate/20190214105400_add_status_to_subscription.rb
+++ db/migrate/20190214105400_add_status_to_subscription.rb
@@ -1,5 +1,5 @@
 class AddStatusToSubscription < ActiveRecord::Migration[5.1]
   def change
-    add_column :subscriptions, :status, :string
+    add_column :subscriptions, :status, :integer, default: 0
   end
 end
