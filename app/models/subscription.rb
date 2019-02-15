3e0a4a6d19fa5e1b71c0a1b25cda3d532e9560fa
LeeSD90 <lee.doyle90@gmail.com>
2019-02-15 12:07:25 +0100

    add new subscription attribute setups to cancel and save methods

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -14,6 +14,9 @@ class Subscription < ApplicationRecord
   def cancel_with_paypal_payment
     if valid?
       response = paypal.cancel_recurring
+      self.expires = self.next_billing_date
+      self.next_billing_date = nil
+      self.status = "Inactive"
       save!
     end
   end
@@ -21,7 +24,10 @@ class Subscription < ApplicationRecord
   def save_with_paypal_payment
     if valid? && paypal_payment_token.present?
       response = paypal.make_recurring
+      puts response.inspect
       self.paypal_recurring_profile_token = response.profile_id
+      self.next_billing_date = Time.now + (self.plan.duration).month
+      self.status = "Active"
       save!
     end
   end
