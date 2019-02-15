96f09595ffee0dc32f16328355cf8724c3d3d34b
LeeSD90 <lee.doyle90@gmail.com>
2019-02-15 12:18:33 +0100

    refactor subscription into some extra methods

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -11,12 +11,20 @@ class Subscription < ApplicationRecord
     PaypalPayment.new(self)
   end
 
+  def update_billing
+    self.next_billing_date = Time.now + (self.plan.duration).month
+  end
+
+  def cancel
+    self.expires = self.next_billing_date
+    self.next_billing_date = nil
+    self.status = "Inactive"
+  end
+
   def cancel_with_paypal_payment
     if valid?
       response = paypal.cancel_recurring
-      self.expires = self.next_billing_date
-      self.next_billing_date = nil
-      self.status = "Inactive"
+      self.cancel
       save!
     end
   end
@@ -24,9 +32,8 @@ class Subscription < ApplicationRecord
   def save_with_paypal_payment
     if valid? && paypal_payment_token.present?
       response = paypal.make_recurring
-      puts response.inspect
       self.paypal_recurring_profile_token = response.profile_id
-      self.next_billing_date = Time.now + (self.plan.duration).month
+      self.update_billing
       self.status = "Active"
       save!
     end
