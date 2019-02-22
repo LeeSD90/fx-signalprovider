e98451d93dfa38820da472489cf71047db25559c
LeeSD90 <lee.doyle90@gmail.com>
2019-02-22 16:17:40 +0100

    add admin_mail method to app mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
-  def test_mail(email)
-    mail(to: email, subject: 'Test mail', body: 'Body Stuff')
+  def admin_mail(subject, body)
+    mail(to: ENV['ADMIN1_MAIL']ENV['ADMIN1_MAIL'], subject: subject, body: body)
   end
 end
